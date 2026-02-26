adb5ab18ae2485bc92aa6f3866029df4c0f235cb
Thaw Hein <imthawhein@gmail.com>
2026-02-26 19:53:16 +0700

    Adding

--- mmnovacity/src/App.jsx
+++ mmnovacity/src/App.jsx
@@ -1,18 +1,20 @@
 import { Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
 
-function Home() {
-  return <h1>Home Page</h1>;
-}
 
-function About() {
-  return <h1>About Page</h1>;
-}
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/contact" element={<Contact />} />
+      
     </Routes>
   );
 }
